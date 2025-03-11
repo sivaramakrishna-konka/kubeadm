28a071837cd51642b244801c14a8ca77f2fa0d05
siva <sivaram0434@gmail.com>
2025-03-11 13:09:35 +0530

    add the terraform required provider info

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.90.0"
+    }
+  }
   backend "s3" {
     bucket = "spa-ecs-example-siva"
     key    = "kubeadm/terraform.tfstate"
