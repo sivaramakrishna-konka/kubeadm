d65c4da43ec47bf4fccd87a31635be8a26875190
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-03-16 21:32:31 +0530

    update the instance size

--- main.tf
+++ main.tf
@@ -74,9 +74,9 @@ resource "aws_security_group" "k8s_sg" {
 # variables
 variable "instance_types" {
   default = {
-    "master"  = "t3a.medium"
-    "worker1" = "t3a.medium"
-    "worker2" = "t3a.medium"
+    "master"  = "t3a.small"
+    "worker1" = "t3a.small"
+    "worker2" = "t3a.small"
   }
 }
 
