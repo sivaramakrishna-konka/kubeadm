db0948d865b080831caece2e8efae491912a3ad8
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-03-16 21:40:47 +0530

    update the bucket

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "spa-ecs-example-siva.pem"
+    bucket = "spa-ecs-example-siva"
     key    = "kubeadm/terraform.tfstate"
     region = "us-east-1"
   }
