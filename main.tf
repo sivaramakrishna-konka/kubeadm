f44ffd8ff20d7e900cb88578a7e5bc8aaa23d2bd
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-03-17 13:21:16 +0530

    update the command

--- main.tf
+++ main.tf
@@ -95,14 +95,14 @@ resource "null_resource" "run_ansible" {
   }
   provisioner "file" {
     content = <<EOT
-      [master1]
-      master ansible_host=127.0.0.1 ansible_connection=local
+    [master1]
+    master ansible_host=127.0.0.1 ansible_connection=local
 
-      [workers]
-      worker1 ansible_host=${aws_instance.k8s_nodes["worker1"].private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/siva ansible_ssh_common_args="-o StrictHostKeyChecking=no"
-      worker2 ansible_host=${aws_instance.k8s_nodes["worker2"].private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/siva ansible_ssh_common_args="-o StrictHostKeyChecking=no"
-      EOT
-          destination = "/home/ubuntu/inventory.ini"
+    [workers]
+    worker1 ansible_host=${aws_instance.k8s_nodes["worker1"].private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/siva ansible_ssh_common_args="-o StrictHostKeyChecking=no"
+    worker2 ansible_host=${aws_instance.k8s_nodes["worker2"].private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/siva ansible_ssh_common_args="-o StrictHostKeyChecking=no"
+    EOT
+    destination = "/home/ubuntu/inventory.ini"
     connection {
       type        = "ssh"
       user        = "ubuntu"
