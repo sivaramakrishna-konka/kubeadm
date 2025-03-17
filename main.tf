c2ca8ce04b2984acb46287186e3025576caf8591
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-03-17 13:14:54 +0530

    update the command

--- main.tf
+++ main.tf
@@ -103,6 +103,12 @@ resource "null_resource" "run_ansible" {
       worker2 ansible_host=${aws_instance.k8s_nodes["worker2"].private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/siva ansible_ssh_common_args="-o StrictHostKeyChecking=no"
       EOT
           destination = "/home/ubuntu/inventory.ini"
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${path.module}/siva")
+      host        = aws_instance.k8s_nodes["master"].public_ip
+    }
   }
   # Execute Ansible
   provisioner "remote-exec" {
